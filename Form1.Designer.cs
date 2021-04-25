b44eaaffbe8028a6020a3b316077eaf76d9f071e
Faruk UZUN <farukqasdfg@hotmail.com>
2021-04-25 14:31:46 +0300

    git klasörü privetten publice çevirildi

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace vizeodevi_son
+namespace deneme
 {
     partial class Form1
     {
@@ -29,21 +29,75 @@ namespace vizeodevi_son
         /// </summary>
         private void InitializeComponent()
         {
+            this.dsasd = new System.Windows.Forms.Label();
+            this.label_durum = new System.Windows.Forms.Label();
+            this.label_yer = new System.Windows.Forms.Label();
+            this.picture_bulutlu = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picture_bulutlu)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dsasd
+            // 
+            this.dsasd.AutoSize = true;
+            this.dsasd.Location = new System.Drawing.Point(157, 254);
+            this.dsasd.Name = "dsasd";
+            this.dsasd.Size = new System.Drawing.Size(38, 15);
+            this.dsasd.TabIndex = 0;
+            this.dsasd.Text = "label1";
+            // 
+            // label_durum
+            // 
+            this.label_durum.AutoSize = true;
+            this.label_durum.Location = new System.Drawing.Point(157, 302);
+            this.label_durum.Name = "label_durum";
+            this.label_durum.Size = new System.Drawing.Size(38, 15);
+            this.label_durum.TabIndex = 1;
+            this.label_durum.Text = "label2";
+            // 
+            // label_yer
+            // 
+            this.label_yer.AutoSize = true;
+            this.label_yer.Location = new System.Drawing.Point(157, 354);
+            this.label_yer.Name = "label_yer";
+            this.label_yer.Size = new System.Drawing.Size(38, 15);
+            this.label_yer.TabIndex = 2;
+            this.label_yer.Text = "label3";
+            // 
+            // picture_bulutlu
+            // 
+            this.picture_bulutlu.Image = global::vizeodevi_son.Properties.Resources._700422_icon_1_cloud_512;
+            this.picture_bulutlu.Location = new System.Drawing.Point(451, 24);
+            this.picture_bulutlu.Name = "picture_bulutlu";
+            this.picture_bulutlu.Size = new System.Drawing.Size(92, 86);
+            this.picture_bulutlu.TabIndex = 3;
+            this.picture_bulutlu.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picture_bulutlu);
+            this.Controls.Add(this.label_yer);
+            this.Controls.Add(this.label_durum);
+            this.Controls.Add(this.dsasd);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picture_bulutlu)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label_derece;
+        private System.Windows.Forms.Label label_durum;
+        private System.Windows.Forms.Label label_yer;
+        private System.Windows.Forms.Label dsasd;
+        private System.Windows.Forms.PictureBox picture_bulutlu;
+        private System.Windows.Forms.PictureBox picture_gunesli;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,17 +7,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
-namespace vizeodevi_son
+using System.Xml;
+using System.Xml.Linq;
+using System.IO;
+namespace deneme
 {
     public partial class Form1 : Form
     {
+        //1912901042-faruk uzun
         public Form1()
         {
             InitializeComponent();
         }
 
+
+
+        private const string api = "7dd2ae243ec7279ff34e3fe8d2954284";
+        private const string baglanti = "http://api.openweathermap.org/data/2.5/weather?q=Aydin&mode=xml&units=metric&APPID=" + api;
+
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+            XDocument hava = XDocument.Load(baglanti);
+
+            var sicaklik = hava.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+            label_derece.Text = sicaklik.ToString() + "°";
+            var durum = hava.Descendants("clouds").ElementAt(0).Attribute("name").Value;
+            label_durum.Text = durum.ToString();
+            var yer = hava.Descendants("city").ElementAt(0).Attribute("name").Value;
+            label_yer.Text = yer.ToString();
+            if (durum.Contains("clear sky") == true)
+
+            {
+
+
+                picture_bulutlu.Visible = false;
+                picture_gunesli.Visible = true;
+            }
+            else
+            {
+                picture_bulutlu.Visible = true;
+                picture_gunesli.Visible = false;
+            }
+
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
         {
 
         }
